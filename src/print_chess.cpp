04e96dd0aa0a721977b8ce7dc56a3092e7b69eed
U-MADMARCHELLO228\Марк <mark1010_00@mail.ru>
2019-04-22 10:36:40 +0700

    Format .cpp and .h clang-format

--- src/print_chess.cpp
+++ src/print_chess.cpp
@@ -2,13 +2,14 @@
 #include <iostream>
 using namespace std;
 
-void printchess(char b[][8]) {
-  for (int i = 0; i < 8; i++) {
-    cout << i + 1 << " ";
-    for (int j = 0; j < 8; j++) {
-      cout << b[i][j] << " ";
+void printchess(char b[][8])
+{
+    for (int i = 0; i < 8; i++) {
+        cout << i + 1 << " ";
+        for (int j = 0; j < 8; j++) {
+            cout << b[i][j] << " ";
+        }
+        cout << endl;
     }
-    cout << endl;
-  }
-  cout << "  a b c d e f g h";
+    cout << "  a b c d e f g h";
 }
