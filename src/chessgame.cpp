b09d2204985abde23f1cf2589aaa9eaf31f7adb5
U-MADMARCHELLO228\Марк <mark1010_00@mail.ru>
2019-05-01 05:21:42 +0700

    Add rules Peshka

--- src/chessgame.cpp
+++ src/chessgame.cpp
@@ -4,8 +4,49 @@
 using namespace std;
 void figure_step_rule(int bukv, int cifra, int bukv1, int cifra1, char b[][8])
 {
-    b[cifra1][bukv1] = b[cifra][bukv];
-    b[cifra][bukv] = ' ';
+    bool peshka;
+    if ((bukv > 8) || (cifra > 8) || (cifra1 > 8) || (bukv1 > 8)) {
+        cout << "Incorrect input, please try again\n";
+        return chessgame(b);
+    }
+    if (b[bukv][cifra] == 'P') {
+        if ((bukv1 == bukv + 1) && (cifra1 == cifra)) {
+            if (b[bukv1][cifra1] == ' ') {
+                b[cifra1][bukv1] = b[cifra][bukv];
+                b[cifra][bukv] = ' ';
+            } else {
+                cout << "Incorrect input, please try again\n";
+                return chessgame(b);
+            }
+        }
+        if ((bukv1 == bukv + 1) && (cifra1 = cifra + 1)) {
+            b[cifra1][bukv1] = b[cifra][bukv];
+            b[cifra][bukv] = ' ';
+        }
+    }
+    if (b[bukv][cifra] == 'R') {
+    }
+    if (b[bukv][cifra] == 'N') {
+    }
+    if (b[bukv][cifra] == 'B') {
+    }
+    if (b[bukv][cifra] == 'Q') {
+    }
+    if (b[bukv][cifra] == 'K') {
+    }
+    // white
+    if (b[bukv][cifra] == 'p') {
+    }
+    if (b[bukv][cifra] == 'r') {
+    }
+    if (b[bukv][cifra] == 'n') {
+    }
+    if (b[bukv][cifra] == 'b') {
+    }
+    if (b[bukv][cifra] == 'q') {
+    }
+    if (b[bukv][cifra] == 'k') {
+    }
 }
 void chessgame(char b[][8])
 {
