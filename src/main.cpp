95fea41f96ef5a9fce9e7d5031ade117a8d13702
U-MADMARCHELLO228\Марк <mark1010_00@mail.ru>
2019-05-01 04:03:24 +0700

    Fixed error main,cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <string>
 #include "chessboard.h"
 #include "chessgame.h"
 #include "print_chess.h"
-#include <iostream>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string>
 using namespace std;
-int main()
-{
-    char b[8][8];
-    chessboard(b);
+int main() {
+  char b[8][8];
+  chessboard(b);
+  printchess(b);
+  while (true) {
+    chessgame(b);
     printchess(b);
-    while (true) {
-        chessgame(b);
-        printchess(b);
-    }
-    return 0;
+  }
+  return 0;
 }
